1077a3fa5ed1c818a8545affce8d177d6762402f
Gael <ack3stx10@gmail.com>
2025-12-03 16:51:33 -0600

    parte 1 hard mode

---
+++ wordle/HardModeViewController.swift
@@ -0,0 +1,67 @@
+//
+//  HardModeViewController.swift
+//  wordle
+//
+//  Created by mac on 03/12/25.
+//
+
+import UIKit
+
+class HardModeViewController: UIViewController {
+    
+    @IBOutlet weak var campo0: UIButton!
+    @IBOutlet weak var campo1: UIButton!
+    @IBOutlet weak var campo2: UIButton!
+    @IBOutlet weak var campo3: UIButton!
+    @IBOutlet weak var campo4: UIButton!
+    @IBOutlet weak var campo5: UIButton!
+    @IBOutlet weak var campo6: UIButton!
+    
+    
+    @IBOutlet weak var campo7: UIButton!
+    @IBOutlet weak var campo8: UIButton!
+    @IBOutlet weak var campo9: UIButton!
+    @IBOutlet weak var campo10: UIButton!
+    @IBOutlet weak var campo11: UIButton!
+    @IBOutlet weak var campo12: UIButton!
+    @IBOutlet weak var campo13: UIButton!
+    
+    @IBOutlet weak var campo14: UIButton!
+    @IBOutlet weak var campo15: UIButton!
+    @IBOutlet weak var campo16: UIButton!
+    @IBOutlet weak var campo17: UIButton!
+    @IBOutlet weak var campo18: UIButton!
+    @IBOutlet weak var campo19: UIButton!
+    @IBOutlet weak var campo20: UIButton!
+    
+    
+    @IBOutlet weak var campo21: UIButton!
+    @IBOutlet weak var campo22: UIButton!
+    @IBOutlet weak var campo23: UIButton!
+    @IBOutlet weak var campo24: UIButton!
+    @IBOutlet weak var campo25: UIButton!
+    @IBOutlet weak var campo26: UIButton!
+    @IBOutlet weak var campo27: UIButton!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
