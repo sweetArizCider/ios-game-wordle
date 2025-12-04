e01d3afd81dfd87ff3259efd45d75e631d99f198
Gael <ack3stx10@gmail.com>
2025-12-03 19:42:42 -0600

    actions de musica

---
+++ wordle/SeleccionarDificultadViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SeleccionarDificultadViewController.swift
+//  wordle
+//
+//  Created by mac on 03/12/25.
+//
+
+import UIKit
+
+class SeleccionarDificultadViewController: UIViewController {
+    
+    
+    @IBOutlet weak var botonAprendiz: UIButton!
+    @IBOutlet weak var botonExperto: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
